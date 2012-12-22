987f04af474ecf516f4fce095e7e10afa70fae5e
mizti <mizugokoro.redirect+github@gmail.com>
2012-12-22 10:42:21 +0900

    Shippo2 init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shippo2::Application.config.secret_token = '5ef1067987085029dc51ce5a4cc34681a8d13bac83020f7e5431714b504aaf58c74877b684076f1fb2161098b238a93f7937af3b1743375177bb93b1399d8310'
